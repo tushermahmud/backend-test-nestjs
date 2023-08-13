70779606e68dbf474db7718c1f716e43334c00ce
Sazzad Mahmud Tusher <sazzadmahmudtusher@Sazzads-MacBook-Pro.local>
2023-08-13 21:32:30 +0600

    fixing dist folder

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(parseInt(process.env.APP_PORT));
+  await app.listen(9090);
 }
 bootstrap();
